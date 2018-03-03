365006a92f016f24d877c06143c560a2e5220386
Bradley Ellert <bellert@dwavesys.com>
2018-03-02 18:19:40 -0800

    Fix cyclic import

--- dwave/embedding/utilities/__init__.py
+++ dwave/embedding/utilities/__init__.py
@@ -1,8 +1,5 @@
 from __future__ import absolute_import
 
-from dwave.embedding.utilities.package_info import __version__, __author__, __authoremail__, __description__
-
 # for each module in this package, import __all__
 
 from dwave.embedding.utilities.utilities import *
-import dwave.embedding.utilities.utilities
